9bbcd8d3ddedf3460a44732e856c4c832256e5bd
cedarwarman <cedardalewarman@gmail.com>
2021-12-30 09:29:49 -0700

    Add print

--- bash/run_pollen_organizing_R_scripts.sh
+++ bash/run_pollen_organizing_R_scripts.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Organizing experiment worksheets into a more computer-friendly format
+printf "\nProcessing worksheets\n"
 Rscript /home/cedar/git/organize_pollen_experiment/R/process_worksheets.R
 
 # Building a pollen-counting sheet from the output of the previous script
+printf "\nBuilding pollen counting sheet\n"
 Rscript /home/cedar/git/organize_pollen_experiment/R/build_pollen_count_sheet.R
+
+printf "\nFinished\n"
